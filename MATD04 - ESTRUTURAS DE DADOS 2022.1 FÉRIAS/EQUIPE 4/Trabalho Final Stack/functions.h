9eafc5ae82e222b47c7411cceefa246fc6532439
Jefferson R <jeffersonraimon@gmail.com>
2022-02-21 08:43:37 -0300

    programa final em C

---
+++ MATD04 - ESTRUTURAS DE DADOS 2022.1 FÉRIAS/EQUIPE 4/Trabalho Final Stack/functions.h
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct NO{
+  float valor;
+  struct NO *proximo;
+} No;
+
+typedef struct PILHA{
+  No *topo;
+  float tamanho;
+} Pilha;
+
+void inicializarPilha(Pilha *pilha) {
+  pilha->topo    = NULL;
+  pilha->tamanho = 0;
+}
+
+
+void empilhar(float valor, Pilha *pilha) {
+  No *novoNo      = malloc(sizeof(No));
+  novoNo->valor   = valor;
+  if (pilha->tamanho == 0)
+    novoNo->proximo = NULL;
+  else
+    novoNo->proximo = pilha->topo;
+  pilha->topo = novoNo;
+  pilha->tamanho++;
+
+}
+
+void exibir(Pilha *pilha) {
+  No *temporario;
+  float soma = 0;
+
+  temporario = pilha->topo;
+
+  int i = pilha->tamanho;
+  while (i > 0) {
+    soma += temporario ->valor;
+    printf("Dia: %i | Valor: [ R$%5.2f ]\n", i, temporario->valor);
+    temporario = temporario->proximo;
+    i--;
+
+
+  }
+  printf("----------------------------\n");
+	printf(" Total MES: R$%5.2f\n", soma);
+
+}
+
+void desempilhar(Pilha *pilha) {
+  if (pilha->tamanho == 0) {
+    printf("Sem Relatorios\n");
+  } else {
+    printf(" VALOR: [R$%5.2f] EXCLUIDO\n", pilha->topo->valor);
+    pilha->tamanho--;
+    pilha->topo = pilha->topo->proximo;
+  }
+}
+
+void limparPilha(Pilha **pilha) {
+  printf("Excluindo Relatorio...\n");
+  No *temporario;
+  temporario = *pilha;
+  while (temporario != NULL) {
+    printf("Excluindo... [R$%5.2f]\n", temporario->valor);
+
+      *pilha = temporario->proximo;
+      free(temporario);
+      temporario = *pilha;
+  }
+}
+
---
+++ MATD04 - ESTRUTURAS DE DADOS 2022.1 FÉRIAS/EQUIPE 4/Trabalho Final Stack/main.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "functions.h"
+
+void inicializarPilha(Pilha *pilha);
+void empilhar(float valor, Pilha *pilha);
+void exibir(Pilha *pilha);
+void desempilhar(Pilha *pilha);
+void limparPilha(Pilha **pilha);
+
+int main() {
+    Pilha pilha;
+    inicializarPilha(&pilha);
+    float novoItem;
+    int opcao = 0, controle = 0;
+
+    do {
+        system("cls");
+        printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
+        printf("       C O N T R O L E - C A I X A\n");
+        printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
+        printf("\n");
+        printf("1. INSERIR TOTAL DO DIA\n");
+        printf("2. IMPRIMIR RELATORIO DIARIO\n");
+        printf("3. EXCLUIR ULTIMA ENTRADA\n");
+		printf("4. EXCLUIR RELATORIO\n");
+        printf("5. SAIR DO PROGRAMA\n");
+        printf("OPCAO: ");
+        scanf(" %d", &opcao);
+
+        switch (opcao) {
+            case 1:
+                do {
+					system("cls");
+                    printf("Total de vendas DIA: \n");
+                    scanf("%f", &novoItem);
+                    empilhar(novoItem, &pilha);
+                    printf("Continuar? [1] - S | [2] - N\n");
+                    scanf(" %d", &controle);
+                } while (controle == 1);
+                break;
+            case 2:
+            	system("cls");
+                exibir(&pilha);
+                system("pause");
+                break;
+
+            case 3:
+				system("cls");
+                desempilhar(&pilha);
+                system("pause");
+                break;
+
+            case 4:
+				system("cls");
+                limparPilha(&pilha);
+                inicializarPilha(&pilha);
+                printf("RELATORIO EXCLUIDO!!!\n");
+                system("pause");
+                break;
+
+        }
+
+    } while (opcao != 5);
+    return 0;
+}
